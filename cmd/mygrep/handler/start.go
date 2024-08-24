bca69e0ce76e70b2589f88894e1d6342781f9907
amol <amlo1232001@gmail.com>
2024-08-25 01:50:49 +0530

    fix start

--- cmd/mygrep/handler/start.go
+++ cmd/mygrep/handler/start.go
@@ -17,6 +17,20 @@ func (s Start) Matches(app *app.App) bool {
 
 func (s Start) Run(app *app.App) bool {
 	l := app.Ptr.LineL
+
+	if l == 0 {
+		s := string(app.Pattern[app.Ptr.PatternL])
+		for app.Ptr.LineL <= app.Ptr.LineR {
+			if s == string(app.Line[app.Ptr.LineL]) {
+				app.Ptr.LineL++
+				return true
+			}
+			app.Ptr.LineL++
+		}
+
+		return false
+	}
+
 	app.Ptr.LineL++
 	return string(app.Pattern[app.Ptr.PatternL]) == string(app.Line[l])
 }
